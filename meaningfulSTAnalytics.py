8d66416822e0e22f940d49687ac3d32748dc07f3
simonscheider <simonscheider@web.de>
2016-11-17 14:27:35 +0100

    initiate

---
+++ meaningfulSTAnalytics.py
@@ -0,0 +1,11 @@
+#-------------------------------------------------------------------------------
+# Name:        Meaningful Spatio-temporal Analytics
+# Purpose:
+#
+# Author:      Simon Scheider
+#
+# Created:     17/11/2016
+# Copyright:   (c) simon 2016
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
